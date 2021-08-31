216c4b75355f9b8b44cd52cdfc67fb26166b7e29
id624373 <Klopfer007>
2021-08-31 18:03:56 +0200

    still WIP

--- src/main/java/gg/oddysian/death/configstorage/Configstorage.java
+++ src/main/java/gg/oddysian/death/configstorage/Configstorage.java
@@ -1,9 +1,11 @@
 package gg.oddysian.death.configstorage;
 
+import gg.oddysian.death.configstorage.commands.CommandHandout;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.io.File;
@@ -16,7 +18,9 @@ import gg.oddysian.death.configstorage.config.TestConfig;
 @Mod(
         modid = Configstorage.MOD_ID,
         name = Configstorage.MOD_NAME,
-        version = Configstorage.VERSION
+        version = Configstorage.VERSION,
+        serverSideOnly = true,
+        acceptableRemoteVersions = "*"
 )
 public class Configstorage {
 
@@ -33,12 +37,18 @@ public class Configstorage {
 
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) throws IOException {
+        log.info("Death's handout plugin has joined the battlefield to give you all the handouts!!!");
         configDir = new File(event.getModConfigurationDirectory() + "//HandoutConfig//");
         configDir.mkdir();
         config = new File(configDir, "config.conf");
         config.createNewFile();
 
 
+    }
+
+    @Mod.EventHandler
+    public void onServerStarting(FMLServerStartingEvent event){
+        event.registerServerCommand(new CommandHandout());
     }
 
     @Mod.EventHandler
--- src/main/java/gg/oddysian/death/configstorage/commands/CommandHandout.java
+++ src/main/java/gg/oddysian/death/configstorage/commands/CommandHandout.java
@@ -4,23 +4,37 @@ import gg.oddysian.death.configstorage.handlers.FileHandler;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
 
-public class handout extends CommandBase {
+public class CommandHandout extends CommandBase {
 
 
     @Override
     public String getName() {
-        return null;
+        return "handout";
     }
 
     @Override
     public String getUsage(ICommandSender sender) {
-        return null;
+        return "/handout";
     }
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+
+        if(sender instanceof EntityPlayerMP)
+            sender = (EntityPlayerMP) sender;
+        else sender.sendMessage(new TextComponentString("Command can only be run by a player!"));
+
+        if (args[0] == "list"){
+
+            //TODO: list argument is supposed to send a message to the sender, listing all kitnames from kits.json
+
+        }
+
+
         //String jsonData = FileHandler.loadConfigFile();
     }
 }
